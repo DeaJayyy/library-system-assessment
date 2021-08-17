26915d385457a0d7bc935b05283f0f33aefca3d7
Dean Jacobsen <42138662+deanosmeanos@users.noreply.github.com>
2021-08-17 11:08:48 +0100

    Updated files

--- libraryassessment/src/library/entities/Library.java
+++ libraryassessment/src/library/entities/Library.java
@@ -56,7 +56,7 @@ public class Library implements Serializable {
 				try (ObjectInputStream LiBrArY_FiLe = new ObjectInputStream(new FileInputStream(lIbRaRyFiLe));) {
 			    
 					SeLf = (Library) LiBrArY_FiLe.readObject();
-					Calendar.gEtInStAnCe().SeT_DaTe(SeLf.lOaN_DaTe);
+					Calendar.getInstance().setDate(SeLf.lOaN_DaTe);
 					LiBrArY_FiLe.close();
 				}
 				catch (Exception e) {
@@ -71,7 +71,7 @@ public class Library implements Serializable {
 	
 	public static synchronized void SaVe() {
 		if (SeLf != null) {
-			SeLf.lOaN_DaTe = Calendar.gEtInStAnCe().gEt_DaTe();
+			SeLf.lOaN_DaTe = Calendar.getInstance().getDate();
 			try (ObjectOutputStream LiBrArY_fIlE = new ObjectOutputStream(new FileOutputStream(lIbRaRyFiLe));) {
 				LiBrArY_fIlE.writeObject(SeLf);
 				LiBrArY_fIlE.flush();
@@ -126,7 +126,7 @@ public class Library implements Serializable {
 
 	public Member aDd_MeMbEr(String lastName, String firstName, String email, int phoneNo) {		
 		Member member = new Member(lastName, firstName, email, phoneNo, gEt_NeXt_MeMbEr_Id());
-		MeMbErS.put(member.GeT_ID(), member);		
+		MeMbErS.put(member.getId(), member);		
 		return member;
 	}
 
@@ -158,13 +158,13 @@ public class Library implements Serializable {
 
 	
 	public boolean cAn_MeMbEr_BoRrOw(Member member) {		
-		if (member.gEt_nUmBeR_Of_CuRrEnT_LoAnS() == lOaNlImIt ) 
+		if (member.getNumberOfCurrentLoans() == lOaNlImIt ) 
 			return false;
 				
-		if (member.FiNeS_OwEd() >= maxFinesOwed) 
+		if (member.finesOwned() >= maxFinesOwed) 
 			return false;
 				
-		for (Loan loan : member.GeT_LoAnS()) 
+		for (Loan loan : member.getLoans()) 
 			if (loan.Is_OvEr_DuE()) 
 				return false;
 			
@@ -173,16 +173,16 @@ public class Library implements Serializable {
 
 	
 	public int gEt_NuMbEr_Of_LoAnS_ReMaInInG_FoR_MeMbEr(Member MeMbEr) {		
-		return lOaNlImIt - MeMbEr.gEt_nUmBeR_Of_CuRrEnT_LoAnS();
+		return lOaNlImIt - MeMbEr.getNumberOfCurrentLoans();
 	}
 
 	
 	public Loan iSsUe_LoAn(Book book, Member member) {
-		Date dueDate = Calendar.gEtInStAnCe().gEt_DuE_DaTe(loanPeriod);
+		Date dueDate = Calendar.getInstance().getDueDate(loanPeriod);
 		Loan loan = new Loan(gEt_NeXt_LoAn_Id(), book, member, dueDate);
-		member.TaKe_OuT_LoAn(loan);
+		member.takeOutLoan(loan);
 		book.BoRrOw();
-		LoAnS.put(loan.GeT_Id(), loan);
+		LoAnS.put(loan.getId(), loan);
 		CuRrEnT_LoAnS.put(book.gEtId(), loan);
 		return loan;
 	}
@@ -198,7 +198,7 @@ public class Library implements Serializable {
 	
 	public double CaLcUlAtE_OvEr_DuE_FiNe(Loan LoAn) {
 		if (LoAn.Is_OvEr_DuE()) {
-			long DaYs_OvEr_DuE = Calendar.gEtInStAnCe().GeT_DaYs_DiFfErEnCe(LoAn.GeT_DuE_DaTe());
+			long DaYs_OvEr_DuE = Calendar.getInstance().getDaysDifference(LoAn.GeT_DuE_DaTe());
 			double fInE = DaYs_OvEr_DuE * FiNe_PeR_DaY;
 			return fInE;
 		}
@@ -211,12 +211,12 @@ public class Library implements Serializable {
 		Book bOoK  = cUrReNt_LoAn.GeT_BoOk();
 		
 		double oVeR_DuE_FiNe = CaLcUlAtE_OvEr_DuE_FiNe(cUrReNt_LoAn);
-		mEmBeR.AdD_FiNe(oVeR_DuE_FiNe);	
+		mEmBeR.addFine(oVeR_DuE_FiNe);	
 		
-		mEmBeR.dIsChArGeLoAn(cUrReNt_LoAn);
+		mEmBeR.dischargeLoan(cUrReNt_LoAn);
 		bOoK.ReTuRn(iS_dAmAgEd);
 		if (iS_dAmAgEd) {
-			mEmBeR.AdD_FiNe(damageFee);
+			mEmBeR.addFine(damageFee);
 			DaMaGeD_BoOkS.put(bOoK.gEtId(), bOoK);
 		}
 		cUrReNt_LoAn.DiScHaRgE();
--- libraryassessment/src/library/entities/Loan.java
+++ libraryassessment/src/library/entities/Loan.java
@@ -26,7 +26,7 @@ public class Loan implements Serializable {
 	
 	public void cHeCk_OvEr_DuE() {
 		if (StAtE == lOaN_sTaTe.CURRENT &&
-			Calendar.gEtInStAnCe().gEt_DaTe().after(DaTe)) 
+			Calendar.getInstance().getDate().after(DaTe)) 
 			this.StAtE = lOaN_sTaTe.OVER_DUE;			
 		
 	}
@@ -37,7 +37,7 @@ public class Loan implements Serializable {
 	}
 
 	
-	public Integer GeT_Id() {
+	public Integer getId() {
 		return LoAn_Id;
 	}
 
@@ -52,8 +52,8 @@ public class Loan implements Serializable {
 
 		StringBuilder sb = new StringBuilder();
 		sb.append("Loan:  ").append(LoAn_Id).append("\n")
-		  .append("  Borrower ").append(MeMbEr.GeT_ID()).append(" : ")
-		  .append(MeMbEr.GeT_LaSt_NaMe()).append(", ").append(MeMbEr.GeT_FiRsT_NaMe()).append("\n")
+		  .append("  Borrower ").append(MeMbEr.getId()).append(" : ")
+		  .append(MeMbEr.getLastName()).append(", ").append(MeMbEr.getFirstName()).append("\n")
 		  .append("  Book ").append(BoOk.gEtId()).append(" : " )
 		  .append(BoOk.gEtTiTlE()).append("\n")
 		  .append("  DueDate: ").append(sdf.format(DaTe)).append("\n")
